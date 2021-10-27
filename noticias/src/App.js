f4da79ca0a60af464976442c25d2f85242e1e45b
yagodurcak <mauridurcak@gmail.com>
2021-10-27 17:47:59 -0300

    un poco mas

---
+++ noticias/src/App.js
@@ -0,0 +1,19 @@
+
+
+function App() {
+  return (
+    <div classNameName="App">
+      <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+        <div className="flex-shrink-0">
+  
+        </div>
+        <div>
+          <div className="text-xl font-medium text-primary">ChitChat</div>
+          <p className="text-gray-500">You have a new message!</p>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
